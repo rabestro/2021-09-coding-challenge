4dfc7f74c602f3c2f36aef0da5fe4ce3073d9aab
Jegors Cemisovs <jegors.cemisovs@outlook.lv>
2021-09-26 10:29:18 +0300

    Add empty test ProductDaoImplTest

--- ecommerce/src/main/java/lv/id/jc/ecommerce/dao/impl/ProductDaoImpl.java
+++ ecommerce/src/main/java/lv/id/jc/ecommerce/dao/impl/ProductDaoImpl.java
@@ -2,11 +2,18 @@ package lv.id.jc.ecommerce.dao.impl;
 
 import lv.id.jc.ecommerce.dao.ProductDao;
 import lv.id.jc.ecommerce.entity.Product;
+import lv.id.jc.ecommerce.repository.EcommerceRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public class ProductDaoImpl implements ProductDao {
+
+    private final EcommerceRepository repository;
+
+    public ProductDaoImpl(final EcommerceRepository repository) {
+        this.repository = repository;
+    }
 
     @Override
     public Optional<Product> getProductById(final int id) {
